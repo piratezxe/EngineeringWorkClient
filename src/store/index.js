928f936bf90662ebb3ba0fd3720c23763586bfcf
Carlos <karol.pisarzewski@gmail.com>
2019-10-24 13:52:28 +0200

    added authentication authorization

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import { alert } from "./alert.module";
+import { authentication } from "./authentication.module";
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+export const store = new Vuex.Store({
+    modules: {
+        alert,
+        authentication
+    }
 });
